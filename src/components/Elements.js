cf576e99b94c750644fad9728181b52135609adb
Curtis Hill <curtis@cstudio.ca>
2023-08-06 10:46:47 -0700

    clean up files

--- src/components/Elements.js
+++ src/components/Elements.js
@@ -3,100 +3,100 @@ import { Link as Route } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Logo = styled(Route)`
-	text-decoration: none;
-	outline: none;
-	border: none;
-	white-space: nowrap;
-	padding: 5px 5px;
-	color: ${({ darkmode }) => (darkmode ? 'var(--light)' : 'var(--dark)')};
-	font-size: var(--h2);
-	font-weight: 500;
-	transition: all 0.15s ease-in-out;
-	cursor: pointer;
-	&:hover {
-		color: var(--hover);
-		transition: 0.15s ease-in-out;
-	}
+  text-decoration: none;
+  outline: none;
+  border: none;
+  white-space: nowrap;
+  padding: 5px 5px;
+  color: ${({ darkmode }) => (darkmode ? 'var(--light)' : 'var(--dark)')};
+  font-size: var(--h2);
+  font-weight: 500;
+  transition: all 0.15s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    color: var(--hover);
+    transition: 0.15s ease-in-out;
+  }
 `;
-
+/*test*/
 export const BtnS = styled(Scroll)`
-	text-decoration: none;
-	outline: none;
-	border: none;
-	white-space: nowrap;
-	background: var(--accent);
-	color: var(--light);
-	margin: 5px 0;
-	padding: 10px 25px;
-	font-size: var(--h3);
-	border-radius: 0px;
-	transition: all 0.15s ease-in-out;
-	cursor: pointer;
-	&:hover {
-		transition: all 0.15s ease-in-out;
-		background: var(--hover);
-	}
+  text-decoration: none;
+  outline: none;
+  border: none;
+  white-space: nowrap;
+  background: var(--accent);
+  color: var(--light);
+  margin: 5px 0;
+  padding: 15px 25px;
+  font-size: var(--h3);
+  border-radius: 25px;
+  transition: all 0.15s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    transition: all 0.15s ease-in-out;
+    background: var(--hover);
+  }
 `;
 
 export const BtnR = styled(Route)`
-	text-decoration: none;
-	outline: none;
-	border: none;
-	white-space: nowrap;
-	background: var(--accent);
-	color: var(--light);
-	min-width: 125px;
-	margin: 5px 0;
-	margin-right: 10px;
-	padding: 10px 25px;
-	font-size: var(--h3);
-	text-align: center;
-	border-radius: 0px;
-	transition: all 0.15s ease-in-out;
-	cursor: pointer;
-	&:hover {
-		transition: all 0.15s ease-in-out;
-		background: var(--hover);
-	}
+  text-decoration: none;
+  outline: none;
+  border: none;
+  white-space: nowrap;
+  background: var(--accent);
+  color: var(--light);
+  min-width: 125px;
+  margin: 5px 0;
+  margin-right: 10px;
+  padding: 15px 25px;
+  font-size: var(--h3);
+  text-align: center;
+  border-radius: 25px;
+  transition: all 0.15s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    transition: all 0.15s ease-in-out;
+    background: var(--hover);
+  }
 `;
 
 export const Section = styled.div`
-	min-height: calc(100vh - var(--header));
-	width: 100%;
-	margin: 0 auto;
-	padding: 50px 0;
-	background: ${(props) => props.background};
+  min-height: calc(100vh - var(--header));
+  width: 100%;
+  margin: 0 auto;
+  padding: 50px 0;
+  background: ${(props) => props.background};
 `;
 
 export const Wrapper = styled.div`
-	max-width: 1000px;
-	margin: 0 auto;
-	padding: 0 15px;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0 15px;
 `;
 
 export const Heading = styled.div`
-	color: var(--light);
-	font-size: var(--h1);
-	padding: 15px 0;
+  color: var(--light);
+  font-size: var(--h1);
+  padding: 15px 0;
 `;
 
 export const Subheading = styled.div`
-	color: var(--light);
-	font-size: var(--h2);
+  color: var(--light);
+  font-size: var(--h2);
 `;
 
 export const Title = styled.div`
-	color: ${(props) => props.color};
-	font-size: var(--h2);
-	text-transform: uppercase;
-	font-weight: 700;
-	letter-spacing: 2px;
+  color: ${(props) => props.color};
+  font-size: var(--h2);
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 2px;
 `;
 
 export const Subtitle = styled.div`
-	color: ${(props) => props.color};
-	font-size: var(--h1);
-	@media screen and (max-width: 500px) {
-		font-size: var(--h2);
-	}
+  color: ${(props) => props.color};
+  font-size: var(--h1);
+  @media screen and (max-width: 500px) {
+    font-size: var(--h2);
+  }
 `;
